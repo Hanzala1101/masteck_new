9a32a662624e3eec904ee2660eff7c61112cafb5
Hanzala1101 <hanzala.612006.it@mhssce.ac.in>
2023-04-07 17:32:28 +0530

    fully working only global variable problem

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from model import model_fun
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
-import speech_recognition as sr
+import speech_recognition as spr
 import base64
 import tempfile
 from pydub import AudioSegment
-import librosa
+import librosa  
 import io
 
 app = Flask(__name__)
@@ -22,41 +22,47 @@ class UserId(db.Model, UserMixin):
     id = db.Column(db.Integer)
     name = db.Column(db.String(100))
 
+Prediction = 0
 
-
-@app.route('/')
+@app.route('/',methods=['POST','GET'])
 def index():
+    if request.method =='POST':
+        return redirect('/login')
+    
     return render_template('register.html')
 
 
-@app.route('/login',methods=['GET','POST'])
+@app.route('/login',methods=['POST','GET'])
 def login():
+    if request.method=='POST':
+        return redirect('/home')
+    
     return render_template('login.html')
 
 
 @app.route('/home')
 def home():
-    predict = model_fun()
-    person_metadata = UserId.query.filter_by(id=predict)
+    person_metadata = UserId.query.filter_by(id=Prediction)
     return render_template('home.html',person_metadata=person_metadata[0].name)
 
 @app.route('/convert', methods=['POST'])
 def convert():
     data = request.get_json()
     audioData = base64.b64decode(data['audio'])
-    # audio_array, sr = librosa.load(io.BytesIO(audioData), sr=41000)
+    audio_array, sr = librosa.load(io.BytesIO(audioData), sr=41000)
     with tempfile.NamedTemporaryFile(suffix='.webm', delete=False) as audioFile:
         audioFile.write(audioData)
         audioFile.flush()
         audio = AudioSegment.from_file(audioFile.name, format='webm')
         wavAudio = tempfile.NamedTemporaryFile(suffix='.wav', delete=False)
         audio.export(wavAudio.name, format='wav')
-        recognizer = sr.Recognizer()
-        audio = sr.AudioFile(wavAudio.name)
+        recognizer = spr.Recognizer()
+        audio = spr.AudioFile(wavAudio.name)
         with audio as source:
             audioData = recognizer.record(source)
         text = recognizer.recognize_sphinx(audioData)
-    # model_fun(audio_array)
+    global Prediction 
+    Prediction = model_fun(audio_array)
     return text
 
 if __name__ == "__main__":
--- model.py
+++ model.py
@@ -1,10 +1,55 @@
-import pickle
 import numpy as np
+from joblib import load
+import librosa
+# import pickle
+# import tensorflow as tf
+# import onnxruntime as rt
 
 def model_fun(audio_array):
-    # model = pickle.load(open('model.pkl', 'rb'))
-    # features = 'hanzala'
+    y, sr = librosa.load(audio_array, mono=True, duration=30)
+    # remove leading and trailing silence
+    y, index = librosa.effects.trim(y)
+    rmse = librosa.feature.rms(y=y)
+    spec_cent = librosa.feature.spectral_centroid(y=y, sr=sr)
+    spec_bw = librosa.feature.spectral_bandwidth(y=y, sr=sr)
+    rolloff = librosa.feature.spectral_rolloff(y=y, sr=sr)
+    zcr = librosa.feature.zero_crossing_rate(y)
+    mfcc = librosa.feature.mfcc(y=y, sr=sr)
+    X_test = [np.mean(rmse), np.mean(spec_cent) ,np.mean(spec_bw), np.mean(rolloff), np.mean(zcr)]
+    for e in mfcc:
+        X_test = X_test.append(np.mean(e))
 
-    # prediction = np.argmax(model.predict(features), axis=-1)
 
-    return 5
+
+    # # Load the saved model joblib
+    model = load('model.joblib')
+
+    # Load the saved model pickle
+    # with open('model.pkl', 'rb') as f:
+    #     model = pickle.load(f)
+
+    # Load the saved model tensorflow
+    # model = tf.keras.models.load_model('saved_model')
+
+    # Load the saved model onnx
+    # sess = rt.InferenceSession("model.onnx")
+
+    # Prepare the input data
+    # input_data = np.array(X_test)
+
+
+    # X_test=np.array([ 0.85275789, -0.37961011, -0.63409832, -0.33713756, -0.22513153,
+    #     1.54746638,  1.00586545, -1.60632633,  0.39062666, -1.01592356,
+    #     0.384919  , -1.75974197,  1.65954197, -0.7531705 , -0.07506431,
+    #     1.55249769, -1.51177434,  0.75661294, -0.64443755,  0.54278893,
+    #    -1.0614548 ,  0.26971105, -1.30321085,  0.12935729,  0.18081706])
+
+
+    # Use the loaded model for predictions
+    predjob = np.argmax(model.predict(X_test), axis=-1)
+    # predpkl = np.argmax(model.predict(X_test), axis=-1)
+    # predtensor = np.argmax(model.predict(X_test), axis=-1)
+    # Run the model to get predictions onnx
+    # predonnx = sess.run(None, {"input": X_test})[0]
+
+    return predjob[0] #, perdonnx[0], predpkl[0], predtensor[0]
